322416b2f82bdfb124f2a863277ed709cef32b3c
Kevin Macfadem <k_macfadem@hotmail.com>
2023-04-16 21:11:24 -0300

    CSS from tracklist and track edited

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ const example = [
   {name: 'name1', artist: 'artist1', album: 'album1'},
   {name: 'name2', artist: 'artist2', album: 'album2'},
   {name: 'name3', artist: 'artist3', album: 'album3'},
+  {name: 'name4', artist: 'artist4', album: 'album4'},
+  {name: 'name5', artist: 'artist5', album: 'album5'},
+  {name: 'name6', artist: 'artist6', album: 'album6'},
+  {name: 'name7', artist: 'artist7', album: 'album7'},
+  {name: 'name8', artist: 'artist8', album: 'album8'},
+  {name: 'name9', artist: 'artist9', album: 'album9'},
+  {name: 'name10', artist: 'artist10', album: 'album10'}
 ];
 
 function App() {
